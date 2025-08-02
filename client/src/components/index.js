a99eb5d4f2a8cd8e54c0a05434b55503bed1a2e1
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-08-02 11:23:14 +0530

    Minor improve : componets exporteing

---
+++ client/src/components/index.js
@@ -0,0 +1,15 @@
+import AuthForm from "./AuthForm";
+import LogList from "./LogList";
+import Navbar from "./Navbar";
+import Layout from "./Layout";
+import ProtectedRoute from "./ProtectedRoute";
+import GoalTracker from "./GoalTracker";
+
+export {
+    AuthForm,
+    LogList,
+    Navbar,
+    Layout,
+    ProtectedRoute,
+    GoalTracker
+};
